95d4cab7a8e7fd2130c826c4ee4ccf46a0798574
yikeber <www.yikebermisganaw@gmail.com>
2024-05-04 22:52:27 +0300

    Topic and Message models and migrations are applied and registered to app

--- Backend/base/views.py
+++ Backend/base/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
 
+from .models import Room
 
-rooms = [
-    {'id':1,'name':'lets learn pytho.'},
-    {'id':2,'name':'Design with me.'},
-    {'id':3,'name':'Frontend developers.'}
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms,}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room = None
-    for room in rooms:
-         if room['id'] == int(pk):
-             room = room
+    room = Room.objects.get(id=pk)
     context = {'room':room,}
              
     return render(request,'base/room.html',context )
