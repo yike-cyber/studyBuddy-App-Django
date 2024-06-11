1cfa30b12aa00c655f6d5b47910fee7ed20ed7f4
= <=>
2024-06-11 14:13:52 +0300

    postgresql db is added

--- studyBud/settings.py
+++ studyBud/settings.py
@@ -70,15 +70,12 @@ WSGI_APPLICATION = 'studyBud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-      
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME':'studybudDb',
-        # 'USER':'yike',
-        # 'PASSWORD':'yikeber50',
-        # # 'HOST': 'localhost',
-        # # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'studyBud',
+        'USER':'yike',
+        'PASSWORD':'yikeber50',
+        'PORT': '5000',
+        'HOST':'localhost'
     }
 }
 
